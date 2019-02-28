21f7362abe3281e01e53ae591b65c2c5ad972862
Alexander Chalk <contact@alexchalk.net>
2019-02-27 19:48:51 -0500

    Add recommended hardware lines from wiki

--- minimal.nix
+++ minimal.nix
@@ -30,6 +30,24 @@
     };
   };
 
+  # Add recommended hardware lines from wiki 
+  hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [
+    (pkgs.stdenv.mkDerivation {
+     name = "broadcom-rpi3bplus-extra";
+     src = pkgs.fetchurl {
+     url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/b518de4/brcm/brcmfmac43455-sdio.txt";
+     sha256 = "0r4bvwkm3fx60bbpwd83zbjganjnffiq1jkaj0h20bwdj9ysawg9";
+     };
+     phases = [ "installPhase" ];
+     installPhase = ''
+     mkdir -p $out/lib/firmware/brcm
+     cp $src $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
+     '';
+     })
+  ];
+  networking.wireless.enable = true;
+
   # Preserve space by sacrificing documentation and history
   services.nixosManual.enable = false;
   nix.gc.automatic = true;
